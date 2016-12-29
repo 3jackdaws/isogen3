dad0f6a7903cd6c2f2f5942d7ff32ee376430105
Ian Murphy <3jackdaws@gmail.com>
2016-12-28 22:24:00 -0800

    URL slashes optional

--- isogen/urls.py
+++ isogen/urls.py
@@ -5,14 +5,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.directory),
-    url(r'directory/', views.directory, name='directory'),
-    url(r'download/', views.downloads, name='downloads'),
+    url(r'directory/?', views.directory, name='directory'),
+    url(r'download/?', views.downloads, name='downloads'),
     url(r'members/?([a-zA-Z0-9]+)?', views.error_ni, name='downloads'),
     url(r'contact', views.error_ni),
     url(r'admin/', admin.site.urls),
 
-    url(r'login/', views.user_login, name='login'),
-    url(r'logout/', views.user_logout, name='logout'),
+    url(r'login/?', views.user_login, name='login'),
+    url(r'logout/?', views.user_logout, name='logout'),
     url(r'error/([0-9]+)', views.error),
     url(r'.', views.error_nf, name='error'),
 ]
