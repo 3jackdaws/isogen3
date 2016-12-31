956dbeb1e0e96a6520897b4fc8eecb15a629e96f
Ian Murphy <3jackdaws@gmail.com>
2016-12-30 18:58:00 -0800

    Fix bottom of page thing

--- apps/shinobu/urls.py
+++ apps/shinobu/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'webhook', views.webhook)
+    url(r'webhook', views.webhook),
+    url(r'kv/(.+)/([a-z0-9/-_]+)?', views.kvstore)
 ]
--- apps/shinobu/views.py
+++ apps/shinobu/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from isogen.views import get_nav_form
+import pymysql
+from isogen.settings import DATABASES
 
 def index(request):
     return HttpResponse("heelo")
@@ -10,3 +12,21 @@ def webhook(request):
         user = request.user
     context = {"title": "Discord Webhook Executor", "login_form": get_nav_form(request), "user": user}
     return render(request, 'shinobu/webhook.html', context)
+
+def kvstore(request, token, key=None):
+    connection = pymysql.Connect(host=DATABASES['kv']['HOST'],
+                                 db=DATABASES['kv']['NAME'],
+                                 user=DATABASES['kv']['USER'],
+                                 password=DATABASES['kv']['PASSWORD'],
+                                 cursorclass=pymysql.cursors.DictCursor)
+    if request.method == 'POST':
+        pass
+    else:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM kv a "
+                       "LEFT JOIN kv b "
+                       "ON a.v = b.k "
+                       "WHERE a.user_id = %s ", (token))
+
+        results= cursor.fetchall()
+        return HttpResponse(str(results))
