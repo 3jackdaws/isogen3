018d7e65592979df53b08ebbc8233c78a896a7c5
Ian Murphy <3jackdaws@gmail.com>
2017-03-04 22:59:29 -0800

    added post intro animation

--- static/isogen/isogen.js
+++ static/isogen/isogen.js
@@ -118,3 +118,41 @@ function fadeInImage(element){
     element.parentNode.style.opacity = 1;
     element.parentNode.removeChild(element);
 }
+
+function scrollToLocation(location, scrollDuration) {
+    var cosParameter = (location - window.scrollY) / 2,
+        scrollCount = 0,
+        oldTimestamp = performance.now();
+    function step (newTimestamp) {
+        scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
+        if (scrollCount >= Math.PI) window.scrollTo(0, location);
+        if (window.scrollY === location) return;
+        window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
+
+
+function doScrolling(elementY, duration) {
+  var startingY = window.pageYOffset
+  var diff = elementY - startingY
+  var start
+
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    // Elapsed miliseconds since start of scrolling.
+    var time = timestamp - start
+    // Get percent of completion in range [0, 1].
+    var percent = Math.min(time / duration, 1)
+
+    window.scrollTo(0, startingY + diff * percent)
+
+    // Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
